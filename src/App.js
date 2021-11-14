81fb3d27db489ffbfe8cf0363bbde3ea009a8d70
Balak282622 <balakpancholi@gmail.com>
2021-11-14 15:19:27 -0500

    Added more Buttons

--- src/App.js
+++ src/App.js
@@ -9,24 +9,37 @@ import Alert from "./components/Alert";
 
 function App() {
   const [mode, setMode] = useState('light'); //if dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1250);
+  }
 
   const  toggleMode = ()=>{
     if (mode === 'light'){
       setMode ('dark')
       document.body.style.backgroundColor = ('#0f1d3e')
+      showAlert('Dark mode is enabled','success')
     }
     else{
       setMode ('light')
       document.body.style.backgroundColor = ('white')
+      showAlert('light mode is enabled','success')
     }
   }
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
       <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode}/>
-      <Alert Alert = "This is alert"/>
+      <Alert alert = {alert}/>
       <div className="container my-3">
-      <TextForm heading="Enter the text to analyze" mode={mode} />
+      <TextForm showAlert = {showAlert} heading="Enter the text to analyze" mode={mode} />
         {/* <About/> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,28 +3,47 @@ import React ,{useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        // console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText)
+        props.showAlert('Converted to upper case','success')
     }
 
     const handleLoClick = ()=>{
-        // console.log("Uppercase was clicked" + text);
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert('Converted to lower case','success')
     }
 
     const handleClear = ()=>{
-        // console.log("Uppercase was clicked" + text);
         let newText = "";
         setText(newText)
+        props.showAlert('text cleared','success')
+    }
+    
+    const handleCapitalize = ()=>{
+        let newText = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase() ;
+        setText(newText)
+        props.showAlert('text capitalized','success')
+    }
+
+    const handleCapitalizeEachWord = ()=>{
+        let newText = text.split(' ')
+        for (let i = 0; i < newText.length; i++) {
+            newText[i] =  newText[i].charAt(0).toUpperCase() + newText[i].slice(1);
+        }
+        setText(newText.join(" "))
+        props.showAlert('each word capitalized','success')
+    }
+    
+    const handleCopy = ()=>{
+        let text = document.getElementById('myBox')
+        text.select();
+        text.setSelectionRange(0,9999)
+        navigator.clipboard.writeText(text.value);
+        props.showAlert('text copied','success')
     }
 
     const handleToWord = ()=>{
-        // console.log("Uppercase was clicked" + text);
-        // const newText = document.createElement('b');
-
-        // setText(newText)
         const element = document.createElement("a");
         const file = new Blob([text],    
             {type: 'text/plain;charset=utf-8'});
@@ -32,10 +51,17 @@ export default function TextForm(props) {
         element.download = "myFile.docx";
         document.body.appendChild(element);
         element.click()
+        props.showAlert('Converted to word file','success')
+    }
+
+    const handleRemoveExtraSpace = () => {
+        let newText = text.split(/[ ]+/)
+        setText(newText.join(" "))
+        props.showAlert('Extra spaces removed','success')
+
     }
 
     const handleOnChange = (event)=>{
-        // console.log("On change")
         setText(event.target.value)
     }
 
@@ -49,16 +75,28 @@ export default function TextForm(props) {
                 <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark' ? 'rgb(15 44 63 / 0.88)' : 'white', color: props.mode === 'dark' ? '#c9f90b' : 'black'}} id="myBox" rows="10"></textarea>
             </div>
             <button className="btn btn-primary m-1" onClick={handleUpClick}>
-                Convert to Uppercase
+                CONVERT TO UPPERCASE
             </button>
             <button className="btn btn-primary m-1" onClick={handleLoClick}>
-                Convert to Lowercase
+                convert to lowercase
+            </button>
+            <button className="btn btn-primary m-1" onClick={handleCapitalize}>
+                Capitalize
+            </button>
+            <button className="btn btn-primary m-1" onClick={handleCapitalizeEachWord}>
+                Capitalize Each Word
             </button>
             <button className="btn btn-primary m-1" onClick={handleClear}>
                 Clear Text
             </button>
+            <button className="btn btn-primary m-1" onClick={handleCopy}>
+                Copy Text
+            </button>
             <button className="btn btn-primary m-1" onClick={handleToWord}>
                 Save to Word file
+            </button>
+            <button className="btn btn-primary m-1" onClick={handleRemoveExtraSpace}>
+                Remove extra spaces
             </button>
         </div>
         <div className="container my-3" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
